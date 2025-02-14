52ffe20d106b3e30d1b5dfa111f21e9e65980663
Victor LEFÈVRE <victor.lefevre@etu.unicaen.fr>
2025-02-14 12:08:03 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template, jsonify
+import pickle
+
+app = Flask(__name__)
+app.debug = True
+
+# Charger les modèles
+cv = pickle.load(open('models/cv.pkl', 'rb'))
+clf = pickle.load(open('models/clf.pkl', 'rb'))
+
+@app.route('/', methods=['GET', 'POST'])
+def predict_email():
+    prediction = None
+    if request.method == 'POST':
+        email = request.form['email']
+        print(email)
+        tokenized_email = cv.transform([email])
+        prediction = clf.predict(tokenized_email)
+        print(prediction)
+    return render_template('input.html', prediction=prediction)
+
+@app.route('/api/predict', methods=['POST'])
+def api_predict():
+    data = request.get_json(force=True)
+    email = data.get('email')
+    tokenized_email = cv.transform([email])
+    prediction = clf.predict(tokenized_email)
+    # Convert prediction to int in case it is in array format.
+    result = {'prediction': int(prediction[0])}
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run()
